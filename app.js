ca98ad7bc35b5f80fed3623b58022c4e7f8f2733
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-05-10 00:20:39 +0530

    made my portfolio using plain html css js

---
+++ app.js
@@ -0,0 +1,13 @@
+let resetbtn= document.querySelector(".sub-btn");
+let mesgs=document.querySelectorAll(".name-pass");
+
+resetbtn.addEventListener("click",() =>{
+    
+    alert("Messege Sent");
+
+
+    //har ek input keliye uska value ko empty krdo
+    mesgs.forEach(input => {
+        input.value="";
+    });
+});
